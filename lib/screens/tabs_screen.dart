73a809675d28d7bfa55d007f184fd44fc6ddd746
Arnab <arnab.bhattacharya28122000@gmail.com>
2020-07-24 16:35:15 +0530

    Initial Commit

---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import '../models/meal.dart';
+import '../widgets/main_drawer.dart';
+import './favourites_screen.dart';
+import './categories_screen.dart';
+
+class TabsScreen extends StatefulWidget {
+  final List<Meal> favouriteMeal;
+
+  TabsScreen(this.favouriteMeal);
+  @override
+  _TabsScreenState createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+
+  List<Map<String, Object>> _pages;
+  int _selectedPageIndex = 0;
+
+  @override
+  void initState(){
+    _pages =[
+    {
+    'page':CategoriesScreen(),
+    'title':'Categories'
+    },
+    {
+      'page':FavouritesScreen(widget.favouriteMeal),
+      'title': 'Your Favourites'
+    }
+    
+  ];
+    super.initState();
+
+  }
+
+  int _selectedPage(int index){
+    setState(() {
+      _selectedPageIndex = index;
+    });
+    return _selectedPageIndex;
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+        appBar: AppBar(
+          title: Text(_pages[_selectedPageIndex]['title']),
+          ),
+          drawer: MainDrawer()
+          ,
+          body:_pages[_selectedPageIndex]['page'],
+          bottomNavigationBar: BottomNavigationBar(
+            onTap: _selectedPage,
+            backgroundColor: Theme.of(context).primaryColor,
+            unselectedItemColor: Colors.white,
+            selectedItemColor: Theme.of(context).accentColor,
+            currentIndex: _selectedPageIndex,
+            type: BottomNavigationBarType.shifting,
+            items: [
+              BottomNavigationBarItem(
+                backgroundColor: Theme.of(context).primaryColor,
+                icon: Icon(Icons.category),
+                title: Text('Categories')
+              ),
+              BottomNavigationBarItem(
+                backgroundColor: Theme.of(context).primaryColor,
+                icon: Icon(Icons.star),
+                title: Text('Favourites')
+              ),
+            ],
+            ),
+    );
+  }
+}
